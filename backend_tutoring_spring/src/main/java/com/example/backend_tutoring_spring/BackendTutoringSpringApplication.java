a9d6597f1da92895deae5665c37be0b347b37d3e
JIOWN <sjwzz0617@naver.com>
2026-04-05 23:45:53 +0900

    다솜 백엔드 2주차 과제

---
+++ backend_tutoring_spring/src/main/java/com/example/backend_tutoring_spring/BackendTutoringSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_tutoring_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTutoringSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTutoringSpringApplication.class, args);
+    }
+
+}
---
+++ backend_tutoring_spring/src/test/java/com/example/backend_tutoring_spring/BackendTutoringSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_tutoring_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTutoringSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
